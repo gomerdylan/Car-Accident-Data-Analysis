4e2646cbb1367220f28021aa1b6ca8d6b0deb464
gomerdylan <gomerdylan@gmail.com>
2021-05-13 19:03:08 -0600

    Implementing User input for data analysis

--- time hist.R
+++ time hist.R
@@ -16,9 +16,22 @@ for (x in 1:length(PeopleInjured)){
 }
 #This creates TimeGivenInjury
 TimeGivenInjury=integer(count)
+k <- 1
 for (x in 1:length(PeopleInjured)){
   if (PeopleInjured[x] > 0){
-    TimeGivenInjury[x] <- DayTime[x]
+    TimeGivenInjury[k] <- DayTime[x]
+    k <- k+1
   }
 }
 hist(TimeGivenInjury, breaks=23)
+UserInput <- function() {
+  while (1==1){
+    my.name <- readline(prompt="Choose a collision type \n 1 for Single Vehicle Crash\n 2 for Rear-end\n 3 for Angle\n 4 for Sideswipe Same Direction\n 5 for Sideswipe Opposite Direction\n 6 for Head on\n 7 for Rear to Rear\n 99 for unknown\n")
+    my.name <<- as.numeric(my.name)
+    if (my.name==1|my.name==3|my.name==4|my.name==5|my.name==6|my.name==7|my.name==99){
+      break
+    }
+    print("That is an invalid entry")
+  }
+}
+UserInput()
